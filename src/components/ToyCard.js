92cbdcd69795b35df722cc65fed1f045629d5505
Dan Koskei <qimtaid@gmail.com>
2024-05-02 22:14:35 +0300

    complete lab

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,33 @@
+import axios from "axios";
 import React from "react";
 
-function ToyCard() {
+function ToyCard({toy, deleteToy, updateToy}) {
+  //const{ id, name, image, likes} = toy;
+
+  function deleteToy(){
+    axios.delete(`http://localhost:3001/toys/${toy.id}`)
+    .then((res)=> deleteToy(res.data))
+  }
+
+  function likeToy(){
+    const updateLikes = {
+      likes: toy.likes + 1,
+    };
+    axios.patch(`http://localhost:3001/toys${toy.id}`,updateLikes)
+    .then((res)=>updateToy(res.data))
+  }
+  
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button className="like-btn" onClick={likeToy}>Like {"<3"}</button>
+      <button className="del-btn" onClick={deleteToy}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
-function ToyForm() {
+function ToyForm({ addToy }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: ""
+  });
+
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const newToy = {
+      ...formData,
+      likes: 0
+    }
+    axios.post("http://localhost:3001/toys", newToy)
+      .then((res) => addToy(res.data))
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          onChange={handleChange}
+          value={formData.name}
           placeholder="Enter a toy's name..."
           className="input-text"
         />
@@ -15,6 +40,8 @@ function ToyForm() {
         <input
           type="text"
           name="image"
+          onChange={handleChange}
+          value={formData.image}
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
